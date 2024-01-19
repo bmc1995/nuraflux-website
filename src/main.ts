d04b3a6d8dc95b748a4abe867620c10e339da3b2
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-01-18 18:25:31 -0700

    ref(styles): add responsiveness

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,46 @@
 import "./style.css";
-import { setupCounter } from "./counter.ts";
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const headings = document.querySelectorAll("article");
+const navbar = document.getElementById("navbar")!;
+const mainContent = document.getElementById("main-content")!;
+
+if (!window.matchMedia("(pointer: coarse)").matches) {
+  document.body.classList.add("hasHover");
+}
+
+const options = {
+  root: mainContent,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+// adjust threshold based on screen size
+if (window.innerWidth < 768) {
+  options.threshold = 0.1;
+} else if (window.innerWidth < 992) {
+  options.threshold = 0.3;
+} else if (window.innerWidth < 1200) {
+  options.threshold = 0.4;
+} else {
+  options.threshold = 0.5;
+}
+
+const callback = (entries: any[], observer: any) => {
+  entries.forEach((entry: IntersectionObserverEntry) => {
+    if (entry.isIntersecting) {
+      const headingId = entry.target.id;
+      const navLink = document.querySelector(`[href="#${headingId}"]`)!;
+      const navUrl = `/#${headingId}`;
+
+      navbar.querySelector(".active")?.classList.remove("active");
+      navLink.classList.add("active");
+      window.history.replaceState(null, "", navUrl);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+headings.forEach((heading) => {
+  observer.observe(heading);
+});
